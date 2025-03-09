e121ee280cc674892d4488b2f210d6e137ab63b0
CauanLagrotta <caun06lagrotta@gmail.com>
2025-03-09 14:35:41 -0300

    fazendo o summary

--- src/styles/global.ts
+++ src/styles/global.ts
@@ -4,6 +4,7 @@ export const GlobalStyle = createGlobalStyle`
     :root{
         --background: #f0f2f5;
         --red: #e52e4d;
+        --green: #33cc95;
         --blue: #5429cc;
 
         --green-light:rgba(57, 184, 40, 0.87);
